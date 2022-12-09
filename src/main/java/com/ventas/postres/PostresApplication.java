44d810602dc393dccd7035fd2ebc81f243f10008
edward.gutierrez <edward.gutierrez.rios@gmail.com>
2022-12-09 15:08:54 -0500

    mi primer commit

---
+++ src/main/java/com/ventas/postres/PostresApplication.java
@@ -0,0 +1,14 @@
+package com.ventas.postres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostresApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PostresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ventas/postres/PostresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventas.postres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
